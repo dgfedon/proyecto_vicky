c078892e057ed73b13ae2aea5caf8f7b31fd34ac
Luz Vergara <luzvergara_16@hotmail.com>
2022-03-31 19:16:41 -0300

    about

--- src/components/pack/PackContainer.jsx
+++ src/components/pack/PackContainer.jsx
@@ -6,6 +6,26 @@ function PackContainer() {
     return (
         <div id="container-pack">
             <h2>Paquetes</h2>
+
+            <div className="paquetes">
+                <div className="package">
+                    <h3> Paquete 1</h3>
+                    <ul>
+                    <li>  Diseño marca </li> 
+                    <li>Manual de usabilidad </li> 
+                    <li>Merchandising </li> 
+                    <li>Cartelería </li> 
+                    <li>Banners </li> 
+                    <li> Flyers </li> 
+                    <li> Ilustraciones </li> 
+                    <li>Vectoriales </li> 
+                    <li>Creación de piezas gráficas para RRHH </li> 
+                    </ul>
+
+                    <button> Contactar</button>
+                </div>
+
+            </div>
         </div>
     )
 }

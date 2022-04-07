8443ef13ba598f772eaabff46754035ee42cdae0
Dariana Garcia <dgarciafedon@gmail.com>
2022-04-06 22:01:38 -0300

    ultimas modificaciones

--- src/components/about/AboutMeContainer.jsx
+++ src/components/about/AboutMeContainer.jsx
@@ -13,16 +13,14 @@ function AboutMeContainer() {
                <img src="/assets/images/Profile.png" alt="profile" />
 
                <p className='main-p'> Mi nombre es Victoria María Argañaraz, 
-                  <span>soy diseñadora gráfica </span>,
-                  elegí esta profesión ya que siempre tuve vocación por la creación 
-                  de <span>elementos visuales </span>, sean de forma tradicional como digital, en 
-                  virtud de ellos estudié diseño que me dio la posibilidad de 
-                  descubrir y experimentar nuevos aspectos de la <span>funcionalidad del 
-                  lenguaje visual </span>, otorgándome conocimientos para adentrarme al 
+                  <span> soy diseñadora gráfica,</span> elegí esta profesión ya que siempre tuve vocación por la creación 
+                  de<span> elementos visuales,</span> sean de forma tradicional como digital, en virtud de ellos estudié diseño que me dio la posibilidad de 
+                  descubrir y experimentar nuevos aspectos de la<span> funcionalidad del 
+                  lenguaje visual,</span> otorgándome conocimientos para adentrarme al 
                   mundo del futuro digital. Ahora ya con mi carrera finalizada tengo 
                   como objetivo seguir trabajando en la creación, como continuar 
                   adquiriendo conocimientos sobre los diversos aspectos de 
-                  <span> creaciones digitales </span>en las distintas áreas.
+                  <span> creaciones digitales</span> en las distintas áreas.
                </p>
 
             </div>
